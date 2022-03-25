882b87c02b9d4a59efd42e636755d8af0124e8f3
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2022-03-25 02:17:37 +0100

    update project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+
+export const BASE_PATH = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  app.setGlobalPrefix(BASE_PATH);
+  app.use(helmet());
+  app.enableCors();
+
   await app.listen(port);
   const url = (await app.getUrl()).replace('[::1]', 'localhost');
   console.log(`Application is running on: ${url}`);
